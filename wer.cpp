b767c4585e0be6c55a59d1df5b08aaf676786e6f
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 21:02:50 +0330

    wer

---
+++ wer.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+    int number;
+
+    // دریافت عدد از ورودی
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    // چاپ مضارب عدد تا 100
+    std::cout << "Multiples of " << number << " up to 100 are: " << std::endl;
+    for (int i = 1; i * number <= 100; ++i) {
+        std::cout << i * number << " ";
+    }
+
+    // خط جدید برای خوانایی بهتر خروجی
+    std::cout << std::endl;
+
+    return 0;
+}
